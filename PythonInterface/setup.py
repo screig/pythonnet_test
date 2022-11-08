1081609af44de60f33ad5a4d96211913a0fe1ac0
sean.creighton <sean.creighton@shell.com>
2022-11-08 10:18:07 +0000

    Refactor

--- PythonInterface/setup.py
+++ PythonInterface/setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+
 # Get the requirements from reading the file.
 dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir_path, 'requirements.txt'), 'r') as rq:
@@ -10,13 +11,8 @@ pkg_dependencies = [req for req in req_list if req.strip()]
 setup(
     name='PythonInterface',
     version='1.0.0',
-    package_dir={'': 'src'},
-    package_data={'src': ['lib/*.dll', 'lib/x64/*.dll', 'lib/x86/*.dll']},
+    packages=['PythonInterface'],
+    package_data={'PythonInterface': ['lib/*.dll', 'lib/x64/*.dll', 'lib/x86/*.dll']},
     include_package_data=True,
-    install_requires=pkg_dependencies,
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-    ]
+    install_requires=pkg_dependencies
 )
--- PythonInterface/tests/test_1.py
+++ PythonInterface/tests/test_1.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-from src import PythonInterface as pyi
+import PythonInterface.PythonInterface as pyi
 
 logger = logging.getLogger(__name__)
 
@@ -17,9 +17,11 @@ class Testing_Class:
     def test_get_time(self):
         logger.info("Get time")
         mytime = pyi.get_time()
-        assert type(mytime) == datetime
+        assert type(mytime) == datetime#
+        logger.info(mytime)
 
     def test_get_names(self):
         logger.info("Get some names")
         names = pyi.get_names()
         assert isinstance(names, list)
+        logger.info(names)
